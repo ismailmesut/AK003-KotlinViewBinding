6a84092491906c4a64dcfeee93e46684a7b75774
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-03 12:29:18 +0300

    updated: view binding

--- app/src/main/java/com/ismailmesutmujde/kotlinviewbinding/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinviewbinding/MainActivity.kt
@@ -2,13 +2,27 @@ package com.ismailmesutmujde.kotlinviewbinding
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinviewbinding.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMainActivity : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMainActivity.root
+        setContentView(view)
+
+        bindingMainActivity.button.setOnClickListener {
+
+        }
+
+        bindingMainActivity.textView.text = "Test"
+        bindingMainActivity.editText.setText("EditText test")
+
 
     }
 }
